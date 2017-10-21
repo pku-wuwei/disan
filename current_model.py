c68263f3a2173c4e0adbed1d1e1be7996c294d75
pku-wuwei <lxww301@gmail.com>
2017-10-21 11:27:55 +0800

    now reusable

--- current_model.py
+++ current_model.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+# v1.1 better reusing
 import tensorflow as tf
 import numpy as np
 
@@ -13,7 +14,7 @@ class Config(object):
     learning_rate = 3e-5
     l2_weight = 1e-7
     penal_weight = 1e-4
-    dropout = 1.0
+    dropout = 0.75
     max_grad = 10
     grad_noise = 0.01
     num_classes = 3
@@ -24,42 +25,51 @@ class Config(object):
     label_list = ['entailment','neutral','contradiction']
 
 
-# sent_emb:[b,n,d]  sent_mask:[b,n]
-def disan(sent_emb, sent_mask, keep_prob):
-    fw_encoding = token2token(sent_emb, sent_mask, True, keep_prob)     # [b,n,d]
-    bw_encoding = token2token(sent_emb, sent_mask, False, keep_prob)
+# sent_hidden:[b,n,d]  sent_mask:[b,n]
+def disan(sent_hidden, sent_mask, keep_prob):
+    fw_encoding = token2token(sent_hidden, sent_mask, True, keep_prob)     # [b,n,d]
+    bw_encoding = token2token(sent_hidden, sent_mask, False, keep_prob)
     sent_encoding = tf.concat([fw_encoding, bw_encoding], 2)
     return source2token(sent_encoding, sent_mask, keep_prob)            # [b,d]
 
-def token2token(sent_emb, sent_mask, is_forward, keep_prob):
-    bs, sl = tf.shape(sent_emb)[0], tf.shape(sent_emb)[1]
-    col, row = tf.meshgrid(tf.range(sl), tf.range(sl))            # [n,n]
-    direction_mask = tf.greater(row, col) if is_forward else tf.greater(col, row)                       # [n,n]
-    direction_mask_tile = tf.tile(tf.expand_dims(direction_mask, 0), [bs, 1, 1])     # [b,n,n]
-    length_mask_tile = tf.tile(tf.expand_dims(sent_mask, 1), [1, sl, 1])             # [b,1,n] -> [b,n,n]
-    attention_mask = tf.logical_and(direction_mask_tile, length_mask_tile)                              # [b,n,n]
+def token2token(sent_hidden, sent_mask, is_forward, keep_prob):
+    scope = 'forward' if is_forward else 'backward'
+    with tf.variable_scope(scope):
+        bs, sl = tf.shape(sent_hidden)[0], tf.shape(sent_hidden)[1]
+        col, row = tf.meshgrid(tf.range(sl), tf.range(sl))            # [n,n]
+        direction_mask = tf.greater(row, col) if is_forward else tf.greater(col, row)                       # [n,n]
+        direction_mask_tile = tf.tile(tf.expand_dims(direction_mask, 0), [bs, 1, 1])     # [b,n,n]
+        length_mask_tile = tf.tile(tf.expand_dims(sent_mask, 1), [1, sl, 1])             # [b,1,n] -> [b,n,n]
+        attention_mask = tf.logical_and(direction_mask_tile, length_mask_tile)                              # [b,n,n]
 
-    sent_hidden = tf.nn.dropout(tf.layers.dense(sent_emb, Config.hidden_size, tf.nn.elu), keep_prob)   # [b,n,d]
-    matching_col = tf.tile(tf.expand_dims(sent_hidden, 1), [1, sl, 1, 1])             # [b,1,n,d] -> [b,n,n,d]
-    matching_row = tf.tile(tf.expand_dims(sent_hidden, 2), [1, 1, sl, 1])             # [b,n,1,d] -> [b,n,n,d]
-    matching_logit = tf.layers.dense(tf.concat([matching_col, matching_row], 3)/5.0, Config.hidden_size, tf.tanh)     # [b,n,n,d]
+        matching_col = tf.tile(tf.expand_dims(sent_hidden, 1), [1, sl, 1, 1])             # [b,1,n,d] -> [b,n,n,d]
+        matching_row = tf.tile(tf.expand_dims(sent_hidden, 2), [1, 1, sl, 1])             # [b,n,1,d] -> [b,n,n,d]
+        matching_logit = 5.0 * dense(tf.concat([matching_col, matching_row], 3)/5.0, Config.hidden_size, tf.tanh, 1.0, 'matching_logit')     # [b,n,n,d]
 
-    matching_logit += tf.expand_dims(1-tf.cast(attention_mask, tf.float32), -1) * (-1e30)               # [b,n,n,d]
-    attention_score = tf.nn.softmax(matching_logit, 2) * tf.expand_dims(tf.cast(attention_mask, tf.float32), -1)    # [b,n,n,d]
-    compare_result = tf.reduce_sum(attention_score * matching_col, 2)                                   # [b,n,d]
+        matching_logit += tf.expand_dims(1-tf.cast(attention_mask, tf.float32), -1) * (-1e30)               # [b,n,n,d]
+        attention_score = tf.nn.softmax(matching_logit, 2) * tf.expand_dims(tf.cast(attention_mask, tf.float32), -1)    # [b,n,n,d]
+        compare_result = tf.reduce_sum(attention_score * matching_col, 2)                                   # [b,n,d]
 
-    fusion_gate = tf.nn.dropout(tf.layers.dense(tf.concat([sent_hidden, compare_result], -1), Config.hidden_size, tf.sigmoid), keep_prob)  # [b,n,d]
-    return fusion_gate*sent_hidden + (1-fusion_gate)*compare_result
-    # tf.expand_dims(tf.cast(sent_mask, tf.float32) -1)   # [b,n,d]
+        fusion_gate = dense(tf.concat([sent_hidden, compare_result], -1), Config.hidden_size, tf.sigmoid, keep_prob, 'fusion_gate')  # [b,n,d]
+        return fusion_gate*sent_hidden + (1-fusion_gate)*compare_result
+        # tf.expand_dims(tf.cast(sent_mask, tf.float32) -1)   # [b,n,d]
 
-def source2token(sent_emb, sent_mask, keep_prob):
-    map1 = tf.nn.dropout(tf.layers.dense(sent_emb, 2*Config.hidden_size, tf.nn.elu), keep_prob)   # [b,n,d]
-    map2 = tf.nn.dropout(tf.layers.dense(map1, 2*Config.hidden_size), keep_prob)   # [b,n,d]
-    map2 += tf.expand_dims(1 - tf.cast(sent_mask, tf.float32), -1) * (-1e30)
-    attention_score = tf.nn.softmax(map2, 1) * tf.expand_dims(tf.cast(sent_mask, tf.float32), -1)
-    return tf.reduce_sum(attention_score * sent_emb, 1)
+def source2token(sent_hidden, sent_mask, keep_prob):
+    with tf.variable_scope('source2token'):
+        map1 = dense(sent_hidden, 2*Config.hidden_size, tf.nn.elu, keep_prob, 'map1')   # [b,n,d]
+        map2 = dense(map1, 2*Config.hidden_size, tf.identity, keep_prob, 'map2')   # [b,n,d]
+        map2 += tf.expand_dims(1 - tf.cast(sent_mask, tf.float32), -1) * (-1e30)
+        attention_score = tf.nn.softmax(map2, 1) * tf.expand_dims(tf.cast(sent_mask, tf.float32), -1)
+        return tf.reduce_sum(attention_score * sent_hidden, 1)
 
-
+def dense(input, out_size, activation, keep_prob, scope):
+    with tf.variable_scope(scope):
+        W = tf.get_variable('W', [input.get_shape()[-1], out_size], dtype=tf.float32)
+        b = tf.get_variable('b', [out_size], dtype=tf.float32)
+        flatten = tf.matmul(tf.reshape(input, [-1, tf.shape(input)[-1]]), W) + b
+        out_shape = [tf.shape(input)[i] for i in range(len(input.get_shape()))]
+        out_shape[-1] = out_size
+        return tf.nn.dropout(activation(tf.reshape(flatten, out_shape)), keep_prob)
 
 class Model(object):
     def __init__(self, embedding):
@@ -84,29 +94,17 @@ class Model(object):
                 self.embed_matrix = tf.convert_to_tensor(self.word_embedding,dtype=tf.float32)
                 self.p_emb = tf.nn.embedding_lookup(self.embed_matrix, self.p_words)      # (b,m,l)
                 self.q_emb = tf.nn.embedding_lookup(self.embed_matrix, self.q_words)      # (b,n,l)
+                with tf.variable_scope("hidden") as scope:
+                    self.p_inp = dense(self.p_emb, Config.hidden_size, tf.nn.elu, self.dropout, 'hidden')   # [b,n,d]
+                    scope.reuse_variables()
+                    self.q_inp = dense(self.q_emb, Config.hidden_size, tf.nn.elu, self.dropout, 'hidden')   # [b,n,d]
+
 
             with tf.variable_scope("disan") as scope:
-                self.p_disan = disan(self.p_emb, self.p_mask, self.dropout)
-            with tf.variable_scope("disanq") as scope:
-                self.q_disan = disan(self.q_emb, self.q_mask, self.dropout)
-            """
-            with tf.variable_scope("encoding_p"):
-                fw_cell = tf.nn.rnn_cell.DropoutWrapper(tf.nn.rnn_cell.GRUCell(Config.hidden_size),input_keep_prob=self.dropout,output_keep_prob=self.dropout)
-                bw_cell = tf.nn.rnn_cell.DropoutWrapper(tf.nn.rnn_cell.GRUCell(Config.hidden_size),input_keep_prob=self.dropout,output_keep_prob=self.dropout)
-                p_outputs, p_states = tf.nn.bidirectional_dynamic_rnn(fw_cell, bw_cell, self.p_emb, self.p_length, dtype=tf.float32)
-                self.p_output = tf.concat(p_outputs, -1)*tf.expand_dims(tf.sequence_mask(self.p_length,tf.shape(self.p_words)[1],tf.float32),-1)   # (b,m,2d)
-                self.p_state = tf.concat(p_states, -1)       # (b,2d)
-                self.p_max = tf.reduce_max(self.p_output, 1)
+                self.p_disan = disan(self.p_inp, self.p_mask, self.dropout)
+                scope.reuse_variables()
+                self.q_disan = disan(self.q_inp, self.q_mask, self.dropout)
 
-
-            with tf.variable_scope("encoding_q"):
-                fw_cell = tf.nn.rnn_cell.DropoutWrapper(tf.nn.rnn_cell.GRUCell(Config.hidden_size),input_keep_prob=self.dropout,output_keep_prob=self.dropout)
-                bw_cell = tf.nn.rnn_cell.DropoutWrapper(tf.nn.rnn_cell.GRUCell(Config.hidden_size),input_keep_prob=self.dropout,output_keep_prob=self.dropout)
-                q_outputs, q_states = tf.nn.bidirectional_dynamic_rnn(fw_cell, bw_cell, self.q_emb, self.q_length, dtype=tf.float32)
-                self.q_output = tf.concat(q_outputs, -1)*tf.expand_dims(tf.sequence_mask(self.q_length,tf.shape(self.q_words)[1],tf.float32),-1)   # (b,m,2d)
-                self.q_state = tf.concat(q_states, -1)       # (b,2d)
-                self.q_max = tf.reduce_max(self.q_output, 1)
-            """
             with tf.variable_scope("loss"):
                 l0 = tf.concat([self.p_disan, self.q_disan], 1)
                 l1 = tf.layers.dense(l0, Config.hidden_size, tf.nn.relu)
